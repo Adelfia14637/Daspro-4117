c42f92e226f4291876a68e514e768eadf00393ca
Adelfia14637 <112890674+Adelfia14637@users.noreply.github.com>
2022-09-08 21:46:34 +0700

    Create tugas.cpp

---
+++ Tugas1/tugas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\t\tHello Dinusian!\n" << endl;
+    cout << "Nama\t\t: Adelfia Milka Novtagravina" << endl;
+    cout << "NIM\t\t: A11.2022.14637" << endl;
+    cout << "Asal Sekolah\t: SMK Telekomunikasi Tunas Harapan Kab.Semarang" << endl;
+    return 0;
+}
