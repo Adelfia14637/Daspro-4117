84706f7a77536681f45d310393003ad8286b6628
Adelfia14637 <112890674+Adelfia14637@users.noreply.github.com>
2022-10-09 22:24:02 +0700

    Add files via upload

---
+++ Pertemuan9/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\t\tTUGAS DASAR PEMROGRAMAN PERTEMUAN 9" << endl;
+    cout << "Nama\t: Adelfia Milka Novtagravina" << endl;
+    cout << "NIM\t: A11.2022.14637" << endl;
+    cout << endl;
+    cout << "Bilangan kelipatan 5 kurang dari 40 adalah sebagai berikut:" << endl;
+
+    int b;
+    b = 5;
+    while (b < 40){
+        cout << b << endl;
+        b+=5;
+    }
+    return 0;
+}
